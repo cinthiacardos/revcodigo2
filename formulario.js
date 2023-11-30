cfcf09b4da1dd3b4f29717635597ffe89379d315
Cinthia Cardos <ccceballos96@gmail.com>
2023-11-29 18:10:23 -0600

    revision de codigo 2 listo

--- formulario.js
+++ formulario.js
@@ -1,40 +1,44 @@
 var formulario = document.querySelector("#form")
 
-formulario.onsubmit = function(e) {
+document.getElementById ("formulario").onsubmit = function(e) { // El codigo no esta bien formulario ya que le faltaba llamar a la funcion GetElementById. se modifico.
 
   e.prevent();
   
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
+  var n = formulario.elements[0]; // se le agrego el punto y coma
+  var e = formulario.elements[1]; // se le agrego el punto y coma
+  var na = formulario.elements[2]; // se le agrego el punto y coma
 
-  var nombre = n.value
-  var edad = e.value
+  var nombre = n.value; // se le agrego el punto y coma
+  var edad = e.value; // se le agrego el punto y coma
 
   var i = na.selectedIndex
   var nacionalidad = na.options[i].value
-  console.log(nombre, edad)
-  console.log(nacionalidad)
+  console.log(nombre, edad); // se le agrego el punto y coma
+  console.log(nacionalidad); // se le agrego el punto y coma
 
   if (nombre.length === 0) {
-    n.classList.add("error")
+    n.classList.add("error"); // se le agrego el punto y coma
   }
   if (edad < 18 || edad > 120) {
-    e.classList.add("error")
+    e.classList.add("error"); // se le agrego el punto y coma
   }
 
 if (nombre.length > 0 
   && (edad > 18 
     && edad < 120) ) {
-  agregarInvitado(nombre, edad, nacionalidad)
-  }
+  agregarInvitado(nombre, edad, nacionalidad); // se le agrego el punto y coma
 }
+  }
 
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-document.body.appendChild(corteLinea)
+
+var botonBorrar = document.createElement("button"); // se le agrego el punto y coma
+
+botonBorrar.textContent = "Eliminar invitado";
+botonBorrar.id = "boton-borrar";
+var corteLinea = document.createElement("br"); // se le agrego el punto y coma
+
+document.body.appendChild(corteLinea); // se le agrego el punto y coma
+
 document.body.appendChild(botonBorrar);
 
 function agregarInvitado(nombre, edad, nacionalidad) {
@@ -52,46 +56,48 @@ function agregarInvitado(nombre, edad, nacionalidad) {
     nacionalidad = "Peruana"
   }
 
-var lista = document.getElementById("lista-de-invitados")
-
-var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
-lista.appendChild(elementoLista)
-
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = "Nombre: "
-inputNombre.value = nombre 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
-
-function crearElemento(descripcion, valor) {
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = descripcion + ": "
-inputNombre.value = valor 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
+var lista = document.getElementById("lista-de-invitados"); // se le agrego el punto y coma
 }
 
-crearElemento("Nombre", nombre)
-crearElemento("Edad", edad)
-crearElemento("Nacionalidad", nacionalidad)
+var elementoLista = document.createElement("div"); // se le agrego el punto y coma
+
+elementoLista.classList.added("elemento-lista"); // se le agrego el punto y coma
+lista.appendChild(elementoLista); // se le agrego el punto y coma
+
+var spanNombre = document.createElement("span"); // se le agrego el punto y coma
+var inputNombre = document.createElement("input"); // se le agrego el punto y coma
+var espacio = document.createElement("br"); // se le agrego el punto y coma
+spanNombre.textContent = "Nombre: "; // se le agrego el punto y coma
+inputNombre.value = nombre; // se le agrego el punto y coma
+elementoLista.appendChild(spanNombre); // se le agrego el punto y coma
+elementoLista.appendChild(inputNombre); // se le agrego el punto y coma
+elementoLista.appendChild(espacio); // se le agrego el punto y coma
+
+function crearElemento(descripcion, valor) {
+var spanNombre = document.createElement("span"); // se le agrego el punto y coma
+var inputNombre = document.createElement("input"); // se le agrego el punto y coma
+var espacio = document.createElement("br");// se le agrego el punto y coma
+spanNombre.textContent = descripcion + ": "
+inputNombre.value = valor 
+elementoLista.appendChild(spanNombre); // se le agrego el punto y coma
+elementoLista.appendChild(inputNombre); // se le agrego el punto y coma
+elementoLista.appendChild(espacio); // se le agrego el punto y coma
+}
+
+crearElemento("Nombre", nombre); // se le agrego el punto y coma
+crearElemento("Edad", edad); // se le agrego el punto y coma
+crearElemento("Nacionalidad", nacionalidad); // se le agrego el punto y coma
 
 
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-elementoLista.appendChild(corteLinea)
+var botonBorrar = document.createElement("button"); // se le agrego el punto y coma
+botonBorrar.textContent = "Eliminar invitado";
+botonBorrar.id = "boton-borrar";
+var corteLinea = document.createElement("br"); // se le agrego el punto y coma
+elementoLista.appendChild(corteLinea); // se le agrego el punto y coma
 elementoLista.appendChild(botonBorrar);
 
  botonBorrar.onclick = function() {
 // this.parentNode.style.display = 'none';
-botonBorrar.parentNode.remove()
+botonBorrar.parentNode.remove(); // se le agrego el punto y coma
   }
 }
